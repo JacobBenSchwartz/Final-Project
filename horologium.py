5961b9e6de66198f0950f6286b8444e30c7eef5f
JacobBenSchwartz <jacob.schwartz1@tufts.edu>
2025-05-03 05:30:17 +0000

    Almost done!

--- horologium.py
+++ horologium.py
@@ -539,7 +539,7 @@ menses = {
         'idus': 13}
         }
 
-#Solution found here: https://stackoverflow.com/questions/28777219/basic-program-to-convert-integer-to-roman-numerals
+# Solution found here: https://stackoverflow.com/questions/28777219/basic-program-to-convert-integer-to-roman-numerals
 num_map = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'),
            (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
 
